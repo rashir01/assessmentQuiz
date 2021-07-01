09e77f545ccdea5c8c7d5ed4d5dd81f3921d5a83
Ray a <rayeed.ashir@gmail.com>
2021-07-01 15:09:44 -0700

    added function to check the answer

--- data.js
+++ data.js
@@ -4,13 +4,16 @@ class Question {
       this.possibleAnswers = possibleAnswers;
       this.rightAnswer = rightAnswer;
     }
+
+    checkAnswer(answerToCheck) {
+        return this.rightAnswer === answerToCheck;
+    }
 }
 
-question = [
+questions = [
     new Question ("Which of the following is an advantage of using JavaScript?", ["A - Less server interaction", "B - Immediate feedback to the visitors", "C - Increased interactivity", "D - All of the above."], "D - All of the above."),
     new Question ("Can you assign a anonymous function to a variable?", ["A - true", "B - false"], "A - true"),
     new Question ("Which built-in method combines the text of two strings and returns a new string?", ["A - append()", "B - concat()", "C - attach()", "D - None of the above."], "B - concat()"), 
     new Question ("Which built-in method returns the calling string value converted to lower case?", ["A - toLowerCase()", "B - toLower()", "C - changeCase(case)", "D - None of the above."], "A - toLowerCase()"), 
     new Question ("Which of the following function of Number object returns a string value version of the current number?", ["A - toString()", "B - toFixed()", "C - toLocaleString()", "D - toPrecision()"], "A - toString()"), 
-
 ];
--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function countdown() {
   var timeInterval = setInterval(function () {
     secondsLeft--;
     mainEl.textContent = secondsLeft ;
-    questionElement.textContent = "Q " + (1 + secondsLeft%5) + ". " + question[secondsLeft%5].questionText;
+    questionElement.textContent = "Q " + (1 + secondsLeft%5) + ". " + questions[secondsLeft%5].questionText;
     if(secondsLeft === 0) {
       clearInterval(timeInterval);
       mainEl.textContent = "Times up!!!!";
@@ -25,21 +25,26 @@ function countdown() {
 
 /*
     TODO: 
+    0. add start button to start the counter
+        0.1 show the button that says start
+        0.2 on click make the button disappear
+        0.3 on click show the counter and the first question
     1. show the timer counting down -DONE
     2. show quizz questions 
         2.1 create quiz object -DONE 
         2.2 quiz object contains a question, up to 4 answers, and a correct answer identifier - DONE
-        2.3 quiz object will have a function that takes an answer and returns true if it matches the correct answer
+        2.3 quiz object will have a function that takes an answer and returns true if it matches the correct answer -DONE
         2.4 display the quiz question and the answers to the screen
-            2.4.1 quiz question should be shown by itesle
+            2.4.1 quiz question should be shown by iteslef
             2.4.2 quiz answers should be shown as a list with radio buttons
         2.5 use a json file to create a list of questions and populate them 
-        
+    
     3. capture quiz answers
     4. when timer expires, show correct answers
     5. prompt for initials 
     6. show highest scores
     7. update CSS
+    
 */
 
 
