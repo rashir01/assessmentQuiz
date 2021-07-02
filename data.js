0fc2e1b8c5179fc3e723a3e542e417f586ac1d93
Ray a <rayeed.ashir@gmail.com>
2021-07-01 19:03:55 -0700

    added all questions rotating on button clicks and capturing the clicked list element

--- data.js
+++ data.js
@@ -11,9 +11,9 @@ class Question {
 }
 
 questions = [
-    new Question ("Which of the following is an advantage of using JavaScript?", ["A - Less server interaction", "B - Immediate feedback to the visitors", "C - Increased interactivity", "D - All of the above."], "D - All of the above."),
-    new Question ("Can you assign a anonymous function to a variable?", ["A - true", "B - false"], "A - true"),
-    new Question ("Which built-in method combines the text of two strings and returns a new string?", ["A - append()", "B - concat()", "C - attach()", "D - None of the above."], "B - concat()"), 
-    new Question ("Which built-in method returns the calling string value converted to lower case?", ["A - toLowerCase()", "B - toLower()", "C - changeCase(case)", "D - None of the above."], "A - toLowerCase()"), 
-    new Question ("Which of the following function of Number object returns a string value version of the current number?", ["A - toString()", "B - toFixed()", "C - toLocaleString()", "D - toPrecision()"], "A - toString()"), 
+    new Question ("Which of the following is an advantage of using JavaScript?", ["Less server interaction", "Immediate feedback to the visitors", "Increased interactivity", "All of the above."], "All of the above."),
+    new Question ("Can you assign a anonymous function to a variable?", ["true", "false"], "true"),
+    new Question ("Which built-in method combines the text of two strings and returns a new string?", ["append()", "concat()", "attach()", "None of the above."], "concat()"), 
+    new Question ("Which built-in method returns the calling string value converted to lower case?", ["toLowerCase()", "toLower()", "changeCase(case)", "None of the above."], "toLowerCase()"), 
+    new Question ("Which of the following function of Number object returns a string value version of the current number?", ["toString()", "toFixed()", "toLocaleString()", "toPrecision()"], "toString()"), 
 ];
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var mainEl = document.getElementById('main');
 var questionElement = document.getElementById('question');
+var answersElement = document.getElementById('answers');
+var count = 0;
 
 /*
     Function countdown
@@ -12,13 +14,51 @@ function countdown() {
   var timeInterval = setInterval(function () {
     secondsLeft--;
     mainEl.textContent = secondsLeft ;
-    questionElement.textContent = "Q " + (1 + secondsLeft%5) + ". " + questions[secondsLeft%5].questionText;
+    
     if(secondsLeft === 0) {
       clearInterval(timeInterval);
       mainEl.textContent = "Times up!!!!";
       //TODO: add code to capture and display the results
     }
   }, 1000);
+}
+
+function addQuestion(questionNumber) {
+
+    questionElement.textContent = "Q " + questions[questionNumber].questionText;
+    for (let i = 0; i < questions[questionNumber].possibleAnswers.length; i++) {
+        var currentPossibleAnswer = questions[questionNumber].possibleAnswers[i];
+        var tag = document.createElement("li");
+        var text = document.createTextNode(currentPossibleAnswer);
+        tag.appendChild(text);
+        answersElement.appendChild(tag);
+        tag.onclick = function (e) {
+            console.log(e.target.textContent); 
+            console.log(questions[count].rightAnswer === e.target.textContent);
+            child = answersElement.lastElementChild; 
+            while (child) {
+                answersElement.removeChild(child);
+                child = answersElement.lastElementChild;
+            }
+            count++;
+            if(count === questions.length) { count = 0;}
+            //checkAnswer
+            addQuestion(count);
+        }
+    }
+
+
+    // var seconds = 60;
+    // var timeInterval = setInterval(function () {
+    //     seconds --;
+    //     questionElement.textContent = "Q " + questions[seconds%5].questionText;
+
+
+    //     if (seconds === 0) {
+    //         clearInterval(timeInterval);
+    //         questionElement.textContent = "No more questions";
+    //     }
+    // }, 5000);
 }
 
 
@@ -34,19 +74,24 @@ function countdown() {
         2.1 create quiz object -DONE 
         2.2 quiz object contains a question, up to 4 answers, and a correct answer identifier - DONE
         2.3 quiz object will have a function that takes an answer and returns true if it matches the correct answer -DONE
-        2.4 display the quiz question and the answers to the screen
-            2.4.1 quiz question should be shown by iteslef
-            2.4.2 quiz answers should be shown as a list with radio buttons
-        2.5 use a json file to create a list of questions and populate them 
+        2.4 display the quiz question and the answers to the screen --DONE
+            2.4.1 quiz question should be shown by iteslef --DONE
+            2.4.2 quiz answers should be shown as a an ordered list --DONE
+        2.5 use a json file to create a list of questions and populate them --DONE
+        2.6 add logic to decide which question to display
     
     3. capture quiz answers
+        3.1 capture clicks on the ul element
+        3.2 switch the question on clicks
     4. when timer expires, show correct answers
     5. prompt for initials 
     6. show highest scores
     7. update CSS
+        7.1 add css to the UL element to change color on mouse over
     
 */
 
 
 countdown();
+addQuestion(count);
 
