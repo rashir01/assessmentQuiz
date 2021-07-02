f25159841800175abdec3843b9553bbf9398e745
Ray a <rayeed.ashir@gmail.com>
2021-07-02 14:56:47 -0700

    added logic to capture the number of correct answers

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var questionElement = document.getElementById('question');
 var answersElement = document.getElementById('answers');
 var count = 0;
 var testIsOver = false;
+var rightAnswers = 0;
 
 /*
     Function countdown
@@ -22,7 +23,7 @@ function countdown() {
                 clearInterval(timeInterval);
                 mainEl.textContent = "";
                 questionElement.textContent = "Time's Up!!!!";
-                answersElement.textContent = "";
+                answersElement.textContent = "You had " + rightAnswers +" correct answers";
             }
             //TODO: add code to capture and display the results
         }
@@ -42,6 +43,9 @@ function addQuestion(questionNumber) {
         tag.onclick = function (e) {
             console.log(e.target.textContent); 
             console.log(questions[count].rightAnswer === e.target.textContent);
+            if(questions[count].rightAnswer === e.target.textContent) {
+                rightAnswers++;
+            }
             child = answersElement.lastElementChild; 
             while (child) {
                 answersElement.removeChild(child);
@@ -51,7 +55,8 @@ function addQuestion(questionNumber) {
             if(count < questions.length) { 
                 addQuestion(count);
             } else {
-                questionElement.textContent = "test is over";
+                questionElement.textContent = "Test is over. "
+                answersElement.textContent = "You had " + rightAnswers + " correct answers";
                 testIsOver = true;
             }
         }
@@ -79,22 +84,25 @@ function addQuestion(questionNumber) {
         0.1 show the button that says start
         0.2 on click make the button disappear
         0.3 on click show the counter and the first question
-    1. show the timer counting down -DONE
-        1.1 stop timer when all questions are answer --DONE
-    2. show quizz questions 
-        2.1 create quiz object -DONE 
-        2.2 quiz object contains a question, up to 4 answers, and a correct answer identifier - DONE
-        2.3 quiz object will have a function that takes an answer and returns true if it matches the correct answer -DONE
-        2.4 display the quiz question and the answers to the screen --DONE
-            2.4.1 quiz question should be shown by iteslef --DONE
-            2.4.2 quiz answers should be shown as a an ordered list --DONE
-        2.5 use a json file to create a list of questions and populate them --DONE
-        2.6 add logic to decide which question to display
+    //1. show the timer counting down -DONE
+    //    1.1 stop timer when all questions are answer --DONE
+    //2. show quizz questions 
+    //    2.1 create quiz object -DONE 
+    //    2.2 quiz object contains a question, up to 4 answers, and a correct answer identifier - DONE
+    //    2.3 quiz object will have a function that takes an answer and returns true if it matches the correct answer -DONE
+    //    2.4 display the quiz question and the answers to the screen --DONE
+    //        2.4.1 quiz question should be shown by iteslef --DONE
+    //        2.4.2 quiz answers should be shown as a an ordered list --DONE
+    //    2.5 use a json file to create a list of questions and populate them --DONE
+    //   2.6 add logic to decide which question to display
         
     
     3. capture quiz answers
-        3.1 capture clicks on the ul element
-        3.2 switch the question on clicks
+        3.1 capture clicks on the ul element --DONE
+        3.2 switch the question on clicks --DONE
+        3.3 track correct answers --DONE
+        3.4 reduce timer on incorrect answers
+
     4. when timer expires, show correct answers
     5. prompt for initials 
     6. show highest scores
